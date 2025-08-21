40f96e7efdf207bbbdb813caddd1394b06bd16d7
m-13747518-jpg <m-13747518@moe-dl.edu.my>
2025-08-21 20:09:58 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+music.play(music.tonePlayable(349, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(330, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
